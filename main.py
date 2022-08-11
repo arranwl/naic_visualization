9a545e44cdc521ec02ae42d7a585318d5eda5f5f
arranwl <92882976+arranwl@users.noreply.github.com>
2022-08-11 12:32:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+#Import the data
 df = pd.read_csv('NAIC_Wage_Data_Median2.csv')
 gdp = pd.read_csv('GDP_DATA_v3.csv')
 gr = pd.read_csv('GROWTH_RATE_v2.csv')
@@ -17,14 +17,15 @@ bc['Geography'] = bc['Geography'].str.strip()
 st.title('NAIC Data Visualization')
 st.sidebar.subheader('Select a data element you would like to visualization')
 
+#Element selection
 element = st.sidebar.selectbox(label='Select an element', options=['GDP','Growth Rate','Wage', 'Business Count', 'Employment'])
 
+#Spacing
 for i in range(3):
     st.text("")
 
 if element == 'Wage':
-    #Search and Select input
-    ptlist = df['GEO'].drop_duplicates().to_list()
+    ptlist = df['GEO'].drop_duplicates().sort_values().to_list()
     ptlist.remove('Canada')
     ptlist.insert(0,'Canada')
     province = st.sidebar.selectbox(label='Select a province', options=ptlist)
@@ -40,7 +41,6 @@ if element == 'Wage':
     scatter.legend(loc=(1,0))
     st.pyplot(fig)
 elif element == 'GDP':
-    # Search and Select input
     years = st.sidebar.multiselect(label='Select years', options=gdp['YEAR'].drop_duplicates(), default=[2021])
 
     data = gdp[gdp['YEAR'].isin(years)]
@@ -54,7 +54,6 @@ elif element == 'GDP':
     scatter.legend(loc=(1, 0))
     st.pyplot(fig)
 elif element == 'Growth Rate':
-    # Search and Select input
     sectors = st.sidebar.multiselect(label='Select sectors', options=gr['NAICS'].drop_duplicates())
 
     data = gr[gr['NAICS'].isin(sectors)]
@@ -67,10 +66,12 @@ elif element == 'Growth Rate':
     scatter.legend(loc=(1, 0))
     st.pyplot(fig)
 elif element == 'Business Count':
-    # Search and Select input
     year = st.sidebar.multiselect(label='Select Year', options=bc['Year'].drop_duplicates().sort_values(ascending=False),
                                   default=[2021])
-    pt = st.sidebar.selectbox(label='Select Region', options=bc['variable'].drop_duplicates())
+    ptlist = bc['variable'].drop_duplicates().sort_values().to_list()
+    ptlist.remove('Canada')
+    ptlist.insert(0, 'Canada')
+    pt = st.sidebar.selectbox(label='Select Region', options=ptlist)
     sizes = st.sidebar.selectbox(label='Select Business Size', options=bc['Size'].drop_duplicates())
 
     data0 = bc[bc['variable'] == pt]
@@ -87,15 +88,19 @@ elif element == 'Business Count':
     scatter.legend(loc=(1, 0))
     st.pyplot(fig)
 elif element == 'Employment':
-    # Search and Select input
     year = st.sidebar.multiselect(label='Select Year',
                                   options=em['YEAR'].drop_duplicates().sort_values(ascending=False),
                                   default=[2021])
-    pt = st.sidebar.selectbox(label='Select Region', options=em['GEO'].drop_duplicates())
+    ptlist = em['GEO'].drop_duplicates().sort_values().to_list()
+    ptlist.remove('Canada')
+    ptlist.insert(0, 'Canada')
+    pt = st.sidebar.selectbox(label='Select Region', options=ptlist)
 
     data = em[em['GEO'] == pt]
     data = data[data['YEAR'].isin(year)]
     data = data[data['NAICS'] != 'Total, all industries']
+    data = data[data['NAICS'] != 'Services-producing sector']
+    data = data[data['NAICS'] != 'Goods-producing sector']
     data = data.sort_values('VALUE')
     data = data[~data['VALUE'].isna()]
 
